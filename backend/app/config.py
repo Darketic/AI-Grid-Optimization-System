a8f0229d8305c4330682edbc05f0308dfcf88c53
Luvya <luvyat0910@gmail.com>
2026-04-12 19:44:44 +0530

    Delete backend directory

--- backend/app/config.py
+++
@@ -1,2 +0,0 @@
-APP_NAME = "AI Grid Optimization System"
-VERSION = "1.0"
--- backend/app/services/prediction_service.py
+++
@@ -1,6 +0,0 @@
-from app.models.random_forest import predict_usage
-
-def analyze_regions(region_data):
-    for region in region_data:
-        region["anomaly"] = predict_usage(region["usage"])
-    return region_data
